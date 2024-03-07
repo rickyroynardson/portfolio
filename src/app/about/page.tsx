ba1463c18257dd80239562d725073b7de4aecdee
Ricky Roy Nardson <rickyroynardson@gmail.com>
2024-03-07 10:39:46 +0700

    about meta

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,30 @@ import { Education } from "@/components/About/Education";
 import { Experience } from "@/components/About/Experience";
 import { Skillset } from "@/components/About/Skillset";
 import { Layout } from "@/components/Layout/Layout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://rickyroyn.vercel.app/"),
+  title: "Ricky Roy Nardson | About",
+  alternates: {
+    canonical: `${process.env.NEXT_PUBLIC_SITE_URL}/about`,
+  },
+  openGraph: {
+    title: "Ricky Roy Nardson | About",
+    description: "My personal portfolio website, check this out!",
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}/about`,
+    type: "website",
+    siteName: "Ricky Roy Nardson | About",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
 
 export default function AboutPage() {
   return (
