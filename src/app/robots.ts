8937aed440b79d16c522a0cd22b8abeef6cbff54
Ricky Roy Nardson <rickyroynardson@gmail.com>
2025-01-15 21:54:24 +0700

    astro initial commit

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-import { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [{ userAgent: "*" }],
-    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
-    host: process.env.NEXT_PUBLIC_SITE_URL,
-  };
-}
