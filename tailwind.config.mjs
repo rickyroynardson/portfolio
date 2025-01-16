0f930dcdd3472bbefecef55d039e8182691dc2ff
Ricky Roy Nardson <rickyroynardson@gmail.com>
2025-01-16 22:42:13 +0700

    add about section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,25 @@ export default {
 			fontFamily: {
 				sans: ["Inter", ...defaultTheme.fontFamily.sans],
 			},
+			keyframes: {
+				wiggle: {
+					"0%, 100%": { transform: "rotate(-3deg)" },
+					"50%": { transform: "rotate(3deg)" },
+				},
+			},
+			animation: {
+				wiggle: "wiggle 0.5s ease-in-out infinite",
+			},
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "1rem",
+				sm: "2rem",
+				lg: "4rem",
+				xl: "5rem",
+				"2xl": "14rem",
+			},
 		},
 	},
 	plugins: [],
